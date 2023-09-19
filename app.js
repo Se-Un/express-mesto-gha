513f381105a486da6a6488e2d7b3caff8cc87cd6
Kim Se-Un <kseun1949@gmail.com>
2023-09-19 12:11:47 +0300

    fix: test 13/9

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const routes = require('./routes');
+const routes = require('./routes/');
 
 const { PORT = 3000, DB_PATH = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
